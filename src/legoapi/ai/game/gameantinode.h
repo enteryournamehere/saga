af1db139bfda94c9ce0c97b4225f5a67a5542555
Avery Townsend <avery@boundaryml.com>
2026-09-20 16:13:41 -0700

    Keep antinode declarations self-contained

--- src/legoapi/ai/game/gameantinode.h
+++ src/legoapi/ai/game/gameantinode.h
@@ -7,8 +7,8 @@ struct GAMEANTINODESYS_s;
 struct GAMEANTINODEDATA_s;
 struct nuvec_s;
 
-GAMEANTINODE_s *GameAntinode_RegisterAntiNode(GAMEANTINODESYS_s *system, nuvec_s *position, f32 radius, f32 extent_x,
-                                              f32 extent_z, u16 angle, i32 shape, f32 duration);
+GAMEANTINODE_s *GameAntinode_RegisterAntiNode(GAMEANTINODESYS_s *system, nuvec_s *position, float radius, float extent_x,
+                                              float extent_z, u16 angle, i32 shape, float duration);
 void GameAntinode_UnregisterAntiNode(GAMEANTINODESYS_s *system, GAMEANTINODE_s *node);
 
 GAMEANTINODE_s *GameAntinode_RegisterAntiNodeUsingData(GAMEANTINODESYS_s *system, nuvec_s *position, u16 angle,
